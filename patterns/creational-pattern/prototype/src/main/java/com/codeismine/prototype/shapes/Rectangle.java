0d1ce5b649dbb50b4f108e7a2e3c8dd41c74f8b4
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-08-01 23:31:06 +0700

    finish prototype pattern

---
+++ patterns/creational-pattern/prototype/src/main/java/com/codeismine/prototype/shapes/Rectangle.java
@@ -0,0 +1,45 @@
+package com.codeismine.prototype.shapes;
+
+/**
+ * Another shape
+ *
+ * @author mengsreang_chhoeung
+ */
+public class Rectangle extends Shape {
+    public int width;
+    public int height;
+    
+    public Rectangle() {
+    }
+    
+    public Rectangle(Rectangle target) {
+        super(target);
+        if (target != null) {
+            this.width = target.width;
+            this.height = target.height;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Rectangle other = (Rectangle) obj;
+        if (this.width != other.width) {
+            return false;
+        }
+        return this.height == other.height;
+    }  
+}
---
+++ patterns/creational-pattern/prototype/src/main/java/com/codeismine/prototype/shapes/Shape.java
@@ -0,0 +1,52 @@
+/**
+ * Shape list
+ */
+
+package com.codeismine.prototype.shapes;
+
+import java.util.Objects;
+
+/**
+ * Common shape interface
+ *
+ * @author mengsreang_chhoeung
+ */
+public abstract class Shape {
+    public int x;
+    public int y;
+    public String color;
+    
+    public Shape() {
+    }
+    
+    public Shape(Shape target) {
+        if (target != null) {
+            this.x = target.x;
+            this.y = target.y;
+            this.color = target.color;
+        }
+    }
+    
+    public abstract Shape clone();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Shape other = (Shape) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return Objects.equals(this.color, other.color);
+    }
+}
