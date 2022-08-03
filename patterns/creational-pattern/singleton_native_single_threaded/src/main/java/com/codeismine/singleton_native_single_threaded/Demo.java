4d8d0072be43cb877e251515d308a5a75c959996
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-08-03 19:57:19 +0700

    finish singleton native-single-threaded pattern

---
+++ patterns/creational-pattern/singleton_native_single_threaded/src/main/java/com/codeismine/singleton_native_single_threaded/Demo.java
@@ -0,0 +1,19 @@
+package com.codeismine.singleton_native_single_threaded;
+
+/**
+ * Client code
+ *
+ * @author mengsreang_chhoeung
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
---
+++ patterns/creational-pattern/singleton_native_single_threaded/src/main/java/com/codeismine/singleton_native_single_threaded/Singleton.java
@@ -0,0 +1,28 @@
+package com.codeismine.singleton_native_single_threaded;
+
+/**
+ * Singleton
+ *
+ * @author mengsreang_chhoeung
+ */
+public class Singleton {
+    private static Singleton instance;
+    public String value;
+    
+    private Singleton(String _value) {
+        // The following code emulates slow initialization
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = _value;
+    }
+    
+    public static Singleton getInstance(String _value) {
+        if (instance == null) {
+            instance = new Singleton(_value);
+        }
+        return instance;
+    }
+}
